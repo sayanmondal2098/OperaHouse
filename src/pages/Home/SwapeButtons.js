53556c71efd2de256e2812b935951e76e343a71a
sayanmondal2098@gmail.com <sayanmondal2098@gmail.com>
2020-08-12 23:48:24 +0530

    primary css done

--- src/pages/Home/SwapeButtons.js
+++ src/pages/Home/SwapeButtons.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, Button } from 'react-native';
 
-import ReplayIcon from "@material-ui/icons/Replay";
-import CloseIcon from "@material-ui/icons/Close";
+
 import StarRateIcon from "@material-ui/icons/StarRate";
 import FavouriteIcon from "@material-ui/icons/Favorite";
 import FlashOnIcon from "@material-ui/icons/FlashOn";
 import IconButton from "@material-ui/core/IconButton";
 import { FontAwesome5, Feather } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { hexToRgb } from '@material-ui/core';
 // import {  } from 're';
 
 // import "./SwapButtons.css";
@@ -22,11 +22,13 @@ class SwapeButtons extends React.Component {
         return (
 
             <SafeAreaView>
-               <FontAwesome5 name="reply" size={38} />
-               <FontAwesome5 name="reply" size={38} />
-               <FontAwesome5 name="reply" size={38} />
-               <FontAwesome5 name="reply" size={38} />
-               <FontAwesome5 name="reply" size={38} />
+                <View >
+                    <View style={style.SwapButtons_repeat}><FontAwesome5 name="reply" size={38} /></View>
+                    <View style={style.SwapButtons_left}><FontAwesome5 name="times-circle" size={38} /></View>
+                    <View style={style.SwapButtons_start}><FontAwesome5 name="star" size={38} /></View>
+                    <View style={style.SwapButtons_right}><FontAwesome5 name="heart" size={38} /></View>
+                    <View style={style.SwapButtons_lightning}><FontAwesome5 name="bolt" size={38} /></View>
+                </View>
             </SafeAreaView>
 
         )
@@ -51,19 +53,19 @@ const style = StyleSheet.create({
         padding: 3
     },
     SwapButtons_left: {
-        color: '#ec5e6f',
+        backgroundColor: 'rgb(236, 94, 111)',//(hexToRgb('#ec5e6f')),
         padding: 3
     },
     SwapButtons_start: {
-        color: '#62b4f9',
+        backgroundColor: 'rgb(98, 180, 249)',
         padding: 3
     },
     SwapButtons_right: {
-        color: '#78e2b3',
+        backgroundColor: 'rgb(120, 226, 179)',
         padding: 3
     },
     SwapButtons_lightning: {
-        color: '#915dd1',
+        backgroundColor: 'rgb(145, 93, 209)',
         padding: 3
     }
 
